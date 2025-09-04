08b61d0b02de8e13b5e10ac74c0083b499e8d33d
lenamhoa <namhoaile115x@gmail.com>
2025-09-04 23:11:43 +0700

    Update index.js

--- scr/index.js
+++ scr/index.js
@@ -3,14 +3,14 @@ export default {
     const target = request.url.split("?target=")[1]
     const cacheKey = "IP_" + target
 
-    let cachedIP = await env.KV.get(cacheKey)
+    let cachedIP = await env["dns-cache-grab"].get(cacheKey)
 
     if (!cachedIP) {
       let dnsRes = await fetch(`https://dns.google/resolve?name=${target}&type=A`)
       let json = await dnsRes.json()
       cachedIP = json.Answer[0].data
 
-      await env.KV.put(cacheKey, cachedIP, { expirationTtl: 1800 }) // TTL 30 phút
+      await env["dns-cache-grab"].put(cacheKey, cachedIP, { expirationTtl: 1800 }) // TTL 30 phút
     }
 
     return new Response(cachedIP)
